fc3d507438ed13cc0b96f0c0aa38d46d09276e74
ajference <37195164+ajference@users.noreply.github.com>
2018-03-08 14:50:47 -0700

    Add files via upload

---
+++ Team.h
@@ -0,0 +1,37 @@
+//----------------------------------------------------------------------//
+// Author:
+// Net ID:
+// Date:
+//
+//----------------------------------------------------------------------//
+
+#ifndef Team_H
+#define Team_H
+
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <iostream>
+#include <queue>
+
+
+class Team{
+private:
+    int teamOffensiveRank;
+    int teamDefensiveRank;
+    
+    int rbRank;
+    int rdDefenseRank;
+    
+    int wrRank;
+    int wrDefenseRank;
+    
+    int teRank;
+    int teDefenseRank;
+    
+public:
+    void Run();
+    
+    
+};
+#endif /* SocialNetworkAnalysis_hpp */
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FantasyFootballPlus
+//
+//  Created by Anthony Ference on 12/16/17.
+//  Copyright © 2017 Anthony Ference. All rights reserved.
+//
+
+#include <iostream>
+#include "Team.h"
+
+int main() {
+    Team Run;
+    Run.Run();
+}
